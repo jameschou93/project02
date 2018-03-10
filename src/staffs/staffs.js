332ba4bb5da2c7d248a596f78ef073587534cacb
James Chou <james.chou.career@gmail.com>
2018-03-10 15:20:46 -0500

    created function to try creating and setting state

--- src/staffs/staffs.js
+++ src/staffs/staffs.js
@@ -10,9 +10,20 @@ class Staffs extends Component {
     ]
   }
 
+  takeme = () => {
+    const newState = this.state.staffs.map((staff) => {
+      const tempStaff = staff;
+      tempStaff.pet = "donkey";
+      return tempStaff
+    })
+
+    this.setState({newState})
+  }
+
   render() {
     return (
       <div>
+      <button onClick={this.takeme}>Do something</button>
         <Staff name={this.state.staffs[0].name}>{this.state.staffs[0].pet}</Staff>
         <Staff name={this.state.staffs[1].name}>{this.state.staffs[1].pet}</Staff>
         <Staff name={this.state.staffs[2].name}>{this.state.staffs[2].pet}</Staff>
