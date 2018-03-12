2e59a1f188f3db4fdaa7c1b623d3715421685644
James Chou <james.chou.career@gmail.com>
2018-03-12 18:55:14 -0400

    binding exercise. user can change displayed text using input field or select state objects

--- src/staffs/staffs.js
+++ src/staffs/staffs.js
@@ -10,7 +10,7 @@ class Staffs extends Component {
     ]
   }
 
-  takeme = () => {
+  donkify = () => {
     const newState = this.state.staffs.map((staff) => {
       const tempStaff = staff;
       tempStaff.pet = "donkey";
@@ -20,13 +20,29 @@ class Staffs extends Component {
     this.setState({newState})
   }
 
+  changeName = (event) => {
+    this.setState({
+      name:event.target.value
+    })
+  }
+  changeName2 = (event) => {
+    this.setState({
+      name:event
+    })
+  }
+
+
+
   render() {
     return (
       <div>
-      <button onClick={this.takeme}>Do something</button>
+      <button onClick={this.donkify}>Do something</button>
+      <input type="text" onChange={this.changeName}/>
         <Staff name={this.state.staffs[0].name}>{this.state.staffs[0].pet}</Staff>
+        <button onClick={this.changeName2.bind(this, this.state.staffs[0].pet)}> bind </button>
         <Staff name={this.state.staffs[1].name}>{this.state.staffs[1].pet}</Staff>
         <Staff name={this.state.staffs[2].name}>{this.state.staffs[2].pet}</Staff>
+        <div> {this.state.name} </div>
       </div>
     )
   }
