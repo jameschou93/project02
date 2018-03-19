c938a0f5e5ba46d4933fea7737b97c73741653f3
James Chou <james.chou.career@gmail.com>
2018-03-19 18:56:15 -0400

    fetched data from hacker news api

--- src/App.js
+++ src/App.js
@@ -3,10 +3,33 @@ import logo from './logo.svg';
 import './App.css';
 import Staffs from './staffs/staffs';
 
+const API = 'https://hn.algolia.com/api/v1/search?query='
+const DEFAULT_QUERY = 'redux'
+
 class App extends Component {
+  state = {
+    hits: [],
+  }
+  
+  componentDidMount() {
+  fetch(API + DEFAULT_QUERY)
+    .then(response => response.json())
+    .then(data => this.setState({ hits: data.hits }));
+  }
+  
+  showMe = () => {
+    console.log(this.state.hits)
+  }
   render() {
+    const { hits } = this.state
     return (
       <div className="App">
+        {hits.map(hit =>
+          <div key={hit.objectID}>
+          <a href={hit.url}>{hit.title}</a>
+          </div>
+        )}
+        <button onClick={this.showMe}> Show me </button>
         <Staffs/>
       </div>
     );
